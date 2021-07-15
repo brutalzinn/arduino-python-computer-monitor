085e66f1e7ddde86f485c476d1dd0407f895bf3a
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-07-15 07:48:43 -0300

    init repository

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -10,12 +10,18 @@
 #include <ArduinoJson.h>
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+int Li          = 16;
+int Lii         = 0; 
+int Ri          = -1;
+int Rii         = -1;
 byte inData;
 char inChar;
 String BuildINString = "";
 char c = "";
 
 boolean NL = true;
+String txtsc,txtnosc;
+
 void setup()
 {
   lcd.init();                      // initialize the lcd
@@ -23,7 +29,18 @@ void setup()
   lcd.clear();
   Serial.begin(9600);
 }
-
+String Scroll_LCD_Left(String StrDisplay){
+  String result;
+  String StrProcess = "                " + StrDisplay + "                ";
+  result = StrProcess.substring(Li,Lii);
+  Li++;
+  Lii++;
+  if (Li>StrProcess.length()){
+    Li=16;
+    Lii=0;
+  }
+  return result;
+}
 void loop()
 {
 
@@ -32,28 +49,33 @@ void loop()
   // when characters arrive over the serial port...
   int size_ = 0;
   String  payload;
-  while (Serial.available()) {
-    delay(100);
-    lcd.clear();
-    payload = Serial.readStringUntil( '\n' );
-    StaticJsonDocument<512> doc;
+  if (Serial.available()) {
+    while (Serial.available()) {
+      lcd.clear();
+      payload = Serial.readStringUntil( '\n' );
+      StaticJsonDocument<512> doc;
 
-    DeserializationError err = deserializeJson(doc, payload);
-    if (err) {
+      DeserializationError err = deserializeJson(doc, payload);
+      if (err) {
+        Serial.flush();
+        return;
+      }
+      String mem = doc["mem"];
+      lcd.setCursor(0, 0);
+      lcd.print("MEM:");
+      lcd.print(Scroll_LCD_Left(mem));
+      lcd.setCursor(0, 1);
+      lcd.print("PROC:");
+      delay(500);
+
       Serial.flush();
-      return;
+      // lcd.print(" ");
+      // lcd.print((char)223);
+      // lcd.print("C");
+
     }
-    String mem = doc["mem"];
-    lcd.setCursor(0, 0);
-    lcd.print("MEM:");
-    lcd.print(mem);
-    lcd.setCursor(0, 1);
-    lcd.print("PROC:");
-    Serial.flush();
-    // lcd.print(" ");
-    // lcd.print((char)223);
-    // lcd.print("C");
-
-
   }
+}
+String ScrollTxt(String txt) {
+  return txt.substring(1, txt.length()) + txt.substring(0, 1);
 }
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from time import sleep
 import serial
+import psutil
+mem = psutil.virtual_memory()
+memPercent = mem.percent
+memTotal = mem.total /1024/1024/1024
+memUsed = mem.active /1024/1024/1024
+cpuPercent = psutil.cpu_percent()
+
+memInfo = f'{memPercent}% {round(memUsed,1)} GB de {round(memTotal,1)} GB'
+
 ser = serial.Serial('/dev/ttyUSB0', 9600) # Establish the connection on a specific port
-ser.write('{"mem":"testeee"}\n'.encode('ascii')) # Convert the decimal number to ASCII then send it to the Arduino
+while True:
+    ser.write(('{"mem":"'+memInfo+'"}\n').encode('ascii')) # Convert the decimal number to ASCII then send it to the Arduino
+    sleep(1)
 ser.close()
