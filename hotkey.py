955951ab551d34add9fc6a5f6905db64834d3fe0
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-07-15 19:35:08 -0300

    added key board support to select type of view

--- hotkey.py
+++ hotkey.py
@@ -1,15 +1,12 @@
 from pynput import keyboard
 
-def on_activate():
-    print('Mostrando uso de memória')
+def activate_mem(on_fix_memory):
 
-def for_canonical(f):
-    return lambda k: f(l.canonical(k))
+    hotkey = keyboard.HotKey(
+        keyboard.HotKey.parse('<ctrl>+<alt>+1'),
+        on_fix_memory)
+    listener = keyboard.Listener(
+            on_press=hotkey.press,
+            on_release=hotkey.release)
+    listener.start()
 
-hotkey = keyboard.HotKey(
-    keyboard.HotKey.parse('<ctrl>+<alt>+1'),
-    on_activate)
-with keyboard.Listener(
-        on_press=for_canonical(hotkey.press),
-        on_release=for_canonical(hotkey.release)) as l:
-    l.join()
--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import serial
 import psutil
 import serial.tools.list_ports
 import GPUtil
-
-
+from hotkey import activate_mem
 ports = serial.tools.list_ports.comports()
 handShakePort = None
+mode = 0
+
 Li = 16
 Lii = 0
-print(type(Li))
 connected = False
+
+
+
 while not connected:
     for port, desc, hwid in sorted(ports):
             print("{}: {} [{}]".format(port, desc, hwid))
@@ -33,34 +36,52 @@ while not connected:
                     break
             except:
                 pass
-def scrollText(text):
-    global Li, Lii
-    result = None
-    StrProcess = "                " + text + "                "
-    result = StrProcess[Li: Lii]
-    Li = Li + 1
-    Lii = Lii + 1
-    if Li > len(StrProcess):
-        Li = 16
-        Lii = 0
-    return result
+
+def setModeValue():
+    global mode
+    mode = 1
+
+activate_mem(setModeValue)
+
 while handShakePort != None:
-    print('startig at port',handShakePort)
+    def modeWriter():
+        global mode
+        print('mode writer called')
+        print('mode',mode)
+        def scrollText(text):
+            global Li, Lii
+            result = None
+            StrProcess = "                " + text + "                "
+            result = StrProcess[Lii: Li]
+            Li = Li + 1
+            Lii = Lii + 1
+            if Li > len(StrProcess):
+                Li = 16
+                Lii = 0
+            return result
+
+        mem = psutil.virtual_memory()
+        memPercent = mem.percent
+        memTotal = mem.total /1024/1024/1024
+        memUsed = mem.active /1024/1024/1024
+        cpuPercent = psutil.cpu_percent()
+        gpu = GPUtil.getGPUs()[0]
+        gpu_util = int(gpu.load * 100)
+        gpu_temp = int(gpu.temperature)
+        cpuTemp = 0
+        cpuTemps = psutil.sensors_temperatures()['coretemp']
+        for item in cpuTemps:
+            if 'Package' in item.label:
+                cpuTemp = item.current
+        memInfo = f'MEM: {memPercent}% {round(memUsed,1)} GB de {round(memTotal,1)} GB'
+        procInfo = f'CPU: {cpuPercent}% {cpuTemp} C GPU: {gpu_util}% {gpu_temp} C'
+        writerResult = '{"rowone":"'+memInfo+'","rowtwo":"'+procInfo+'"}\n'
+        if mode == 1:
+            writerResult = '{"rowone":"'+scrollText(memInfo)+'","rowtwo":"'+procInfo+'"}\n'
+        return writerResult
+    #print('startig at port',handShakePort)
     ser = serial.Serial(handShakePort, 9600)
-    mem = psutil.virtual_memory()
-    memPercent = mem.percent
-    memTotal = mem.total /1024/1024/1024
-    memUsed = mem.active /1024/1024/1024
-    cpuPercent = psutil.cpu_percent()
-    gpu = GPUtil.getGPUs()[0]
-    gpu_util = int(gpu.load * 100)
-    gpu_temp = int(gpu.temperature)
-    cpuTemp = 0
-    cpuTemps = psutil.sensors_temperatures()['coretemp']
-    for item in cpuTemps:
-        if 'Package' in item.label:
-            cpuTemp = item.current
-    memInfo = f'MEM: {memPercent}% {round(memUsed,1)} GB de {round(memTotal,1)} GB'
-    procInfo = f'CPU: {cpuPercent}% {cpuTemp} C GPU: {gpu_util}% {gpu_temp} C'
-    ser.write(('{"rowone":"'+scrollText(memInfo)+'","rowtwo":"'+procInfo+'"}\n').encode('ascii'))
+
+    ser.write((modeWriter()).encode('ascii'))
     sleep(1)
+
